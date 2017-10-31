2c5867b96e9b6e5c14a4f62e1373e479cf2178f3
Sharan Yalburgi <sharanyalburgi@gmail.com>
2017-10-31 21:34:45 +0530

    Fix user login
    * add .gitignore
    
    * edit .gitignore
    
    * add basic login
    
    * add login
    
    * add login success

--- swd/swd/settings.py
+++ swd/swd/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'swd.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, "templates"),
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,4 +125,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,  'static'),
+]
+
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
