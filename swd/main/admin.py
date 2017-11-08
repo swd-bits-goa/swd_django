9d4c98258f988faa593e5d35041698b9fee7bfdf
Sebastin Santy <sebastinssanty@gmail.com>
2017-11-08 23:56:45 +0530

    Add all models to admin

--- swd/main/admin.py
+++ swd/main/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from main.models import Student
+from main.models import *
 
 class StudentAdmin(admin.ModelAdmin):
     search_fields = ['name','bitsId']
+
+models = [ Faculty, Warden, Nucleus, Superintendent, FacultyIncharge, Staff, DayScholar, HostelPS, CSA, MessOption, Bonafide, Leave, DayPass, LateComer, InOut, Disco, MessOptionOpen, Transaction, MessBill]
     
 admin.site.register(Student, StudentAdmin)
+admin.site.register(models)
