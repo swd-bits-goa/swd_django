92d27ed8af4ac13e0ff38b764b2e1a272e244089
Sharan Yalburgi <sharanyalburgi@gmail.com>
2017-12-24 23:10:32 +0530

    Fixed currentUser GraphQL query

--- swd/swd/middleware.py
+++ swd/swd/middleware.py
@@ -1,6 +1,6 @@
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
-
+# currently not being used, the middleware is giving errors
 class JWTMiddleware(object):
     def process_view(self, request, view_func, view_args, view_kwargs):
         token = request.META.get('HTTP_AUTHORIZATION', '')
