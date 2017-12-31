a4dd90048b8afa46d58c7edf8609216557bf134d
Adithya Narayanan <n.adithya10197@gmail.com>
2017-12-31 13:11:44 +0530

    App specific urls moved to main/urls.py (#17)

--- swd/swd/urls.py
+++ swd/swd/urls.py
@@ -19,52 +19,38 @@ from tools import user, profile
 from django.contrib.auth import views as auth_views
 from django.conf.urls.static import static
 from django.conf import settings
-from main import views as main_views
 from graphene_django.views import GraphQLView
 from schema.schema import schema
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView
-from main.views import HelloPDFView
 
 # REST
 from rest_framework_jwt.views import obtain_jwt_token
 from rest_framework_jwt.views import refresh_jwt_token
 from rest_framework_jwt.views import verify_jwt_token
 
+
 urlpatterns = [
     url(r'^jet/', include('jet.urls', 'jet')),
     url(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
     url(r'^admin/', admin.site.urls),
 
-    
+
     url(r'^graphql', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
     url(r'^gql', csrf_exempt(GraphQLView.as_view(batch=True, schema=schema))),
-    
+
 #    url(r'^login/', auth_views.login, {'template_name': 'admin/login.html'}),
 #    url(r'^logout/', auth_views.logout),
 
-    # Django Login
-    url(r'^login/', main_views.loginform, name="login"),
-    url(r'^logout/', main_views.logoutform, name="logout"),
-
-    url(r'^', include('main.urls')),
-    url(r'^create-users/', user.index, name='user'),
-    url(r'^create-profiles/', profile.index, name='profile'),
-    url(r'^accounts/profile/', main_views.login_success, name='login-success'),
-
-
-    # REST
+    #REST
     url(r'^api-token-auth/', obtain_jwt_token),
     url(r'^api-token-refresh/', refresh_jwt_token),
     url(r'^api-token-verify/', verify_jwt_token),
 
-    url(r'^dashboard/', main_views.dashboard, name="dashboard"),
-    url(r'^profile/', main_views.profile, name="profile"),
-    url(r'^messoption/', main_views.messoption, name="messoption"),
-    url(r'^leave/', main_views.leave, name="leave"),
-    url(r'^certificates/', main_views.certificates, name="certificates"),
-    url(r'^bonafidepdf/', main_views.bonafidepdf, name="bonafidepdf"),
 
-    url('bonafide/', HelloPDFView.as_view()),
+    url(r'^', include('main.urls')),
+    url(r'^create-users/', user.index, name='user'),
+    url(r'^create-profiles/', profile.index, name='profile'),
+
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
