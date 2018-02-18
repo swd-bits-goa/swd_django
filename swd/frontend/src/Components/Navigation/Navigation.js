87b75562790e431a427c9b997da4eedd61297e5f
Gaurav Sangle <gauravrsan@gmail.com>
2018-02-18 21:11:43 +0530

    Completed Student Search! Backend- Graphene endpoints. Frontend- ReactUI

--- swd/frontend/src/Components/Navigation/Navigation.js
+++ swd/frontend/src/Components/Navigation/Navigation.js
@@ -21,6 +21,11 @@ import s from './Navigation.css';
 import logoUrl from './logo-small.png';
 import LoginModal from './LoginModal';
 import { Mobile } from '../Responsive';
+import Search from '../Search/Search.js';
+import SearchBar from '../Search/SearchBar.js';
+import {Link, BrowserRouter, Route} from 'react-router-dom';
+import Layout from "../Layout/Layout";
+import closeIcon from './close.svg';
 
 class Navigation extends React.Component {
   static propTypes = {
@@ -33,15 +38,40 @@ constructor(props) {
   super(props);
   this.state = {
     loginModalOpen : false,
-    sidebarOpen: false
+    sidebarOpen: false,
+    open: false,
+    search: false,
+    putSearch: " "
   };
   this.handleSideBarToggle = this.handleSideBarToggle.bind(this);
+  this.getSearch = this.getSearch.bind(this);
 }
+
   handleSideBarToggle = () => {
     this.setState( prevState => ({
   sidebarOpen: !prevState.sidebarOpen
 }));
 }
+    
+  componentWillMount(){
+    window.location.pathname==='/Search'?this.setState({search: true}):this.setState({search: false});
+  }
+  
+  getSearch = (e) => {
+    this.setState({putSearch: e});
+  }
+
+  handleSideClick = () => {
+    this.setState({ open: !this.state.open});
+  }
+
+  handleSearch = () => {
+    this.setState({ search: true });
+  }
+
+  handleCloseSearch = () => {
+    this.setState({ search: false });
+  }
 
   handleLoginOpen = () => {
     this.setState({ loginModalOpen: true });
@@ -69,21 +99,24 @@ constructor(props) {
       // There's a noticeable lag when rendering components based on
       // media queries for the first time
       <Mobile>
+      <BrowserRouter>
+      <div>
         <div className={s.AppBar}>
           <Toolbar>
             <ToolbarGroup firstChild>
               <IconButton onClick={this.handleSideBarToggle}><NavigationMenu color={darkGreen} /></IconButton>
               <Sidebar open={this.state.sidebarOpen} toggleOpen={this.handleSideBarToggle} />
-              <ToolbarTitle text="SWD" />
-              <ToolbarSeparator />
+              {this.state.search?<SearchBar style={{width: '90%'}} getSearch={this.getSearch}/>:<ToolbarTitle text="SWD" />}
+              {this.state.search?<span/>:<ToolbarSeparator style={{marginLeft:0}} />}
             </ToolbarGroup>
-            <ToolbarGroup lastChild>
-              <IconButton iconStyle={filledIcon}><ActionSearch color={darkGreen} /></IconButton>
-              { !(this.props.isLoggedIn) ? 
-              <RaisedButton label="Login" backgroundColor={darkGreen} labelColor={grey50} onTouchTap={this.handleLoginOpen} />
+            <ToolbarGroup lastChild style={{position: 'relative'}}>
+              {this.state.search?<span/>:<Link to='/Search' style={{position: 'absolute', left: 5}}><IconButton iconStyle={filledIcon} style={{paddingLeft: 0, paddingRight: 20}} onClick={this.handleSearch}><ActionSearch color={darkGreen} /></IconButton></Link>}
+              {this.state.search?<span/>:<ToolbarSeparator style={{position: 'absolute', left: 20}}/>}
+              {!this.state.search? !(this.props.isLoggedIn) ? 
+              <FlatButton label="Login" onTouchTap={this.handleLoginOpen}  style={{paddingLeft: 20}}/>
               :
-             <RaisedButton label="Logout" backgroundColor={darkGreen} labelColor={grey50} onTouchTap={this.handleLogout} />
-              }
+             <FlatButton label="Logout" onTouchTap={this.handleLogout}  style={{paddingLeft: 20}}/>
+              :<FlatButton onClick={this.handleCloseSearch} style={{position: 'relative', left: 20}}><img src={closeIcon} style={{position: 'relative', left: 5, height: 20}}/></FlatButton>}
 
             </ToolbarGroup>
             
@@ -93,6 +126,13 @@ constructor(props) {
             onRequestClose={this.handleLoginClose}
             login={this.props.login} />
         </div>
+        <div>
+          <Route
+              path="/Search"
+              render={()=>(<Search search={this.state.putSearch}/>)}/>
+        </div>
+      </div>
+      </BrowserRouter>
       </Mobile>
     );
 
