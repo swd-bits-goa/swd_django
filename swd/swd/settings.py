e9f2a0d379112e6edd655dc130ba60b192bc3146
Sebastin Santy <sebastinssanty@gmail.com>
2018-02-21 14:33:25 +0530

    Prod changes for collectstatic Error (#61)

--- swd/swd/settings.py
+++ swd/swd/settings.py
@@ -182,10 +182,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static/admin'
+STATIC_ROOT = 'static'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,  'static'),
     os.path.join(BASE_DIR,  'assets'),
 ]
 
