6a67798a9c859da4eafb0b7ee8dfbdca439c0f4e
Sebastin Santy <sebastinssanty@gmail.com>
2018-02-22 17:16:41 +0530

    Provision to print mess wise complete data

--- swd/main/views.py
+++ swd/main/views.py
@@ -8,7 +8,6 @@ from datetime import date, datetime, timedelta
 from .forms import MessForm, LeaveForm, BonafideForm, DayPassForm
 from django.contrib import messages
 from django.utils.timezone import make_aware
-import datetime
 
 from braces import views
 
@@ -451,27 +450,40 @@ def messbill(request):
         days = end_date - start_date
         days = days.days + 1
 
+        if request.POST.get('mess') is not 'N':
+            values = MessOption.objects.filter(mess=request.POST.get(
+                'mess'), monthYear=start_date.replace(day=1))
         for k in values:
-            obj = Student.objects.get(bitsId=k)
+            if request.POST.get('mess') is not 'N':
+                obj = k.student
+            else:
+                obj = Student.objects.get(bitsId=k)
+                try:
+                    mess = MessOption.objects.get(
+                        student=obj, monthYear=start_date.replace(day=1))
+                except:
+                    mess = MessOption.objects.create(
+                        student=obj, monthYear=start_date.replace(day=1), mess='N')
+
             leaves = Leave.objects.filter(student=obj)
-            try:
-                mess = MessOption.objects.get(student=obj, monthYear=start_date.replace(day=1))
-            except:
-                mess = MessOption.objects.create(student=obj, monthYear=start_date.replace(day=1), mess='N')
-            
+
             noofdays = 0
 
             for leave in leaves:
                 if leave.approved == True:
                     if leave.dateTimeStart.date() >= start_date and leave.dateTimeStart.date() <= end_date and leave.dateTimeEnd.date() >= end_date:
-                        noofdays += abs(end_date - leave.dateTimeStart.date()).days + 1
+                        noofdays += abs(end_date -
+                                        leave.dateTimeStart.date()).days + 1
                     elif leave.dateTimeEnd.date() >= start_date and leave.dateTimeEnd.date() <= end_date and leave.dateTimeStart.date() <= start_date:
-                        noofdays += abs(leave.dateTimeEnd.date() - start_date).days + 1
+                        noofdays += abs(leave.dateTimeEnd.date() -
+                                        start_date).days + 1
                     elif leave.dateTimeStart.date() >= start_date and leave.dateTimeEnd.date() <= end_date:
-                        noofdays += abs(leave.dateTimeEnd.date() - leave.dateTimeStart.date()).days + 1
+                        noofdays += abs(leave.dateTimeEnd.date() -
+                                        leave.dateTimeStart.date()).days + 1
                     elif leave.dateTimeStart.date() <= start_date and leave.dateTimeEnd.date() >= end_date:
                         noofdays += abs(end_date - start_date).days + 1
             finalamt = amount * days - rebate * noofdays
+
             row = [
                 obj.name,
                 obj.bitsId,
@@ -479,18 +491,24 @@ def messbill(request):
                 rebate * noofdays,
                 finalamt
             ]
-            if mess.mess == 'A':
+
+            if request.POST.get('mess') is not 'N':
                 row_num += 1
                 for col_num in range(len(row)):
                     ws.write(row_num, col_num, row[col_num], font_style)
-            elif mess.mess == 'C':
-                row_num_2 += 1
-                for col_num in range(len(row)):
-                    ws2.write(row_num_2, col_num, row[col_num], font_style)
             else:
-                row_num_3 += 1
-                for col_num in range(len(row)):
-                    ws3.write(row_num_3, col_num, row[col_num], font_style)
+                if mess.mess == 'A':
+                    row_num += 1
+                    for col_num in range(len(row)):
+                        ws.write(row_num, col_num, row[col_num], font_style)
+                elif mess.mess == 'C':
+                    row_num_2 += 1
+                    for col_num in range(len(row)):
+                        ws2.write(row_num_2, col_num, row[col_num], font_style)
+                else:
+                    row_num_3 += 1
+                    for col_num in range(len(row)):
+                        ws3.write(row_num_3, col_num, row[col_num], font_style)
 
         wb.save(response)
         return response
