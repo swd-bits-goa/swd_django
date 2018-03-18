a5c4bd2dfd826f8f364e9650a3b5344fe025228d
Aryan Agarwal <f2016705@goa.bits-pilani.ac.in>
2018-03-18 23:30:42 +0530

    Changes in LoggedIn.js

--- swd/frontend/src/Routes/loggedIn/LoggedIn.js
+++ swd/frontend/src/Routes/loggedIn/LoggedIn.js
@@ -1,15 +1,28 @@
 import React from "react";
 import {Mobile} from "../../Components/Responsive";
-import ExpandableCardViewMore from "../../Components/ExpandableCardViewMore/ExpandableCardViewMore";
 import ExpandableCard from "../../Components/ExpandableCard";
-
 
 class LoggedIn extends React.Component {
 
     render() {
         return (
             <Mobile>
-               <ExpandableCardViewMore title={"hello"} text={"hello"} cardText={"Yo"}/>
+                <ExpandableCard title={<p>Your current mess option is <b>A</b> dining hall till <b>30th, September,2017.</b></p> }/>
+                <ExpandableCard title={<p>Your total dues are <b>RS. 124</b></p>}
+                                text={<p>   Rs. 300 : Waves polo tee<br/>
+                                            Rs. 345 : Waves pass<br/>
+                                    Rs. 1285 : Medical facilities</p>
+                }/>
+                <div>
+                <ExpandableCard title={<p>Your leave has been granted</p>} text={<p>Leave ID: <b>1234</b><br/>
+                    Leave from: <b>21/04/18</b><br/>
+                    Leave till: <b>23/01/18</b><br/>
+                    Warden approval: <b>Yes</b>
+                </p>}/>
+                 {/*TODO: ADD info card here from main screen*/}
+
+
+                </div>
             </Mobile>
 
         );
