10de3965f5c044ce3db086f23ac0fa35079ed846
Gaurav Sangle <gauravrsan@gmail.com>
2018-03-30 12:00:39 +0530

    Users Current Certificates using username

--- swd/frontend/src/Routes/Certificates/Certificates.js
+++ swd/frontend/src/Routes/Certificates/Certificates.js
@@ -4,6 +4,10 @@ import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+import UserCertificates from './UserCertificates.js';
+import {graphql} from 'react-apollo';
+import gql from 'graphql-tag';
+
 
 const styles = {
     container: {
@@ -36,19 +40,27 @@ const styles = {
     }
 }
 
-export default class Certificates extends React.Component{
+class Certificates extends React.Component{
     constructor(){
         super();
         this.state = {
             value: 1,
+            username: ""
           };
     }
     handleChange = (event, index, value) => this.setState({value});
     render(){
+        const { loading, error, currentUser } = this.props.data;
+        if(loading)
+            return <p>loading</p>
+        else if(error)
+            return <p>Error</p>
+        if(!currentUser)
+            return <h3>You need to login first</h3>
         return(
             <div style={styles.container}>
             <Paper zDepth={1} style={styles.formContainer}>
-                <h2 style={styles.header}>Apply for bonafide certificate</h2>
+                <h2 style={styles.header}>Apply for bonafide certificate {currentUser.username}</h2>
                 <SelectField
                     floatingLabelText="Reason"
                     hintText="Reason"
@@ -67,16 +79,41 @@ export default class Certificates extends React.Component{
                     multiLine={true}/>
                 <RaisedButton label="Submit" primary={true} style={styles.submitButton} />
             </Paper>
-            <Paper zDepth={1} style={styles.formContainer}>
-                <h2 style={styles.header}>Bonafide Applications</h2>
-                <div style={{display: 'flex'}}>
-                    <div>No.</div>
-                    <div>Reason</div>
-                    <div>Status</div>
-                </div>
-            </Paper>
+            <UserCertificates username={currentUser.username}/>
             </div>
         )
     }
 }
+
+/*const UserCertificates = (props) => {
+    const {loading, error, bonafide } = props.data;
+    if(loading)
+        return <p>loading</p>
+    if(error)
+        return <p>error</p>
+    return bonafide.map((bon) => {
+        return (
+            <Paper zDepth={1} style={styles.formContainer}>
+                <h2 style={styles.header}>Bonafide Applications</h2>
+                <div style={{display: 'flex'}}>
+                    <div>No.</div>
+                    <div>{bon.reason}</div>
+                    <div>{bon.status}</div>
+                </div>
+            </Paper>
+        )
+    })
+}*/
+
+
+
+const currentUser = gql`
+    query currentUser{
+        currentUser{
+            username
+        }
+    }
+`;
+
+export default graphql(currentUser)(Certificates);
 
