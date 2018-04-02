5741c805f002b4104e8f5d1fafc260c571546b49
Prudhvi Rampey <prudhvi.rampey@gmail.com>
2018-04-02 17:35:44 +0530

    Add CardContentLoader

--- swd/frontend/src/Components/Loaders/Loaders.js
+++ swd/frontend/src/Components/Loaders/Loaders.js
@@ -4,6 +4,7 @@ import {Card} from 'material-ui/Card';
 import ExpandableCard from '../ExpandableCard';
 import LinearProgress from 'material-ui/LinearProgress';
 import CircularProgress from 'material-ui/CircularProgress';
+import ContentLoader, {Facebook, Code, List} from 'react-content-loader'
 
 // Import custom footer styles
 import s from './Loaders.css';
@@ -15,12 +16,20 @@ const style = {
   },
 };
 
-function CardLoader() {
+function CardContentLoader() {
   return (
     <div className={s.container}>
 <Card containerStyle={style.center}>
   {/* <LinearProgress mode="indeterminate" /> */}
-   <CircularProgress size={60} thickness={7} />
+   {/* <CircularProgress size={60} thickness={7} /> */}
+    <ContentLoader
+    height={50}
+    speed={0.5}
+>
+    {/* Pure SVG */}
+    <rect x="0" y="0" rx="4" ry="4" width="400" height="16"/>
+    <rect x="0" y="28" rx="3" ry="3" width="350" height="13"/>
+  </ContentLoader>
    
   </Card>
   </div>
@@ -28,4 +37,4 @@ function CardLoader() {
   )
 }
 
-export {CardLoader};
+export {CardContentLoader};
