baf8fb4f0d4406300fb788918901c8ceaf88a2d6
Mehul Mohan <mehulmpt@gmail.com>
2018-04-15 20:43:13 +0530

    Multiple features added

--- swd/swd/config.py
+++ swd/swd/config.py
@@ -2,3 +2,4 @@ PRODUCTION = False
 DB_NAME = 'swd' if PRODUCTION == True else ''
 DB_USER = 'swduser' if PRODUCTION == True else ''
 DB_PASSWORD = 'swduser' if PRODUCTION == True else ''
+EMAIL_PROD = False
