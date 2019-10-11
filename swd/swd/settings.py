5f70ef8caad9a34a782f04c18ad234e6e71fb842
ashadhaz <ashad.123@hotmail.co.uk>
2019-10-11 15:44:33 +0530

    fixed css in admin page

--- swd/swd/settings.py
+++ swd/swd/settings.py
@@ -187,7 +187,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/assets/'
 STATIC_ROOT = 'static'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
