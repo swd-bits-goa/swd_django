f401eca9c568d3d5632dcbd952706dd68ea39807
ashadhaz <ashad.123@hotmail.co.uk>
2019-11-10 14:42:05 +0530

    fixed uploading profile pictures

--- swd/tools/images.py
+++ swd/tools/images.py
@@ -1,36 +1,21 @@
-from .dev_info import info
-import shutil
 import requests
 from main.models import Student
 from django.core.files import File
 from pathlib import Path
 from django.http import HttpResponse
-'''
-def index(request):
-    for inf in info:
-        bitsId = inf[0][:-1]
-        if Path('../ProfilePictures/' + bitsId[:12] + '.jpg').is_file():
-            print(bitsId + 'Exists')
-        else:
-            pass
+import os
+
+def insert(requests):
+    ctr = 0
+    if os.path.isdir("ProfilePictures"):
+        for i in os.listdir("ProfilePictures"):
             try:
-                url = 'https://swd.bits-goa.ac.in/css/studentImg/' + bitsId[:12] + '.jpg'
-                response = requests.get(url, stream=True, verify=False)
-                with open('../ProfilePictures/' + bitsId[:12] + '.jpg', 'wb') as out_file:
-                    shutil.copyfileobj(response.raw, out_file)
-                    print(bitsId)
-                del response
+                student = Student.objects.get(bitsId=i[:13])
+                student.profile_picture.save(i , File(open('ProfilePictures/' + i, 'rb')))
+                ctr= ctr+1
             except Exception as e:
                 print(e)
                 print('fail')
-    return HttpResponse("Images downloaded")
-'''
-def insert(requests):
-    for inf in info:
-        print(inf[0][:-1])
-        try:
-            student = Student.objects.get(bitsId=inf[0])
-            student.profile_picture.save(inf[0][:-1] + '.jpg', File(open('../ProfilePictures/' + inf[0][:-1] + '.jpg', 'rb')))
-        except Exception as e:
-            print(e)
-            print('fail')
+        return HttpResponse("Number of images added = " + str(ctr))
+    else:
+        return HttpResponse("No such directory named ProfilePictures found.")
