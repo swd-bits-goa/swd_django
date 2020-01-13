c69a61afe03be452a446e35e7ec03c74dd6ce8a2
root <root@newswd.bits-goa.ac.in>
2020-01-13 12:02:57 +0530

    settings.py changed for csrf

--- swd/swd/settings.py
+++ swd/swd/settings.py
@@ -24,6 +24,7 @@ from tools.dev_info import EMAIL_HOST_PASSWORD
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+CSRF_TRUSTED_ORIGINS = ['newswd.bits-goa.ac.in', 'swd.bits-goa.ac.in']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -32,7 +33,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 if PRODUCTION:
-    ALLOWED_HOSTS = ['10.10.10.121', 'newswd.bits-goa.ac.in']
+    ALLOWED_HOSTS = ['10.10.10.121', 'newswd.bits-goa.ac.in', 'swd.bits-goa.ac.in']
 else:
     ALLOWED_HOSTS = []
 
