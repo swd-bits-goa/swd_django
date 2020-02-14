11498d095d0e7dec57259af265eac111da2874b0
ashadhaz <ashad.123@hotmail.co.uk>
2020-02-14 12:27:37 +0530

    final chaanges

--- swd/swd/config.py
+++ swd/swd/config.py
@@ -1,4 +1,4 @@
-PRODUCTION = False
+PRODUCTION = True
 DB_NAME = 'swd' if PRODUCTION == True else ''
 DB_USER = 'swduser' if PRODUCTION == True else ''
 DB_PASSWORD = 'swduser' if PRODUCTION == True else ''
