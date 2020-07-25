6cbe53bc623cfe2ad606aae6f1b4e6be3cece908
raghav_dhir <raghav@musicperk.com>
2020-07-25 14:40:55 +0530

    Added logo and alignment in hostelsuperintendent

--- swd/swd/config.py
+++ swd/swd/config.py
@@ -1,5 +1,5 @@
-PRODUCTION = False
+PRODUCTION = True
 DB_NAME = 'swd' if PRODUCTION == True else ''
 DB_USER = 'swduser' if PRODUCTION == True else ''
 DB_PASSWORD = 'swduser' if PRODUCTION == True else ''
-EMAIL_PROD = False
+EMAIL_PROD = True
