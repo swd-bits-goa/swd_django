4a70c860b1596feb634371471a99126573c0e6c8
ashadhaz <ashad.123@hotmail.co.uk>
2020-08-04 12:47:01 +0530

    added gate security app

--- swd/swd/settings.py
+++ swd/swd/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'webpack_loader',
     'main',
+    'gate_security',
     'tools',
     'schema',
     'graphene_django',
--- swd/swd/urls.py
+++ swd/swd/urls.py
@@ -49,6 +49,7 @@ urlpatterns = [
 
 
     url(r'^', include('main.urls')),
+    url(r'^', include('gate_security.urls')),
 
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
