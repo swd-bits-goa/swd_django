5a231c44a6d0e9984badcf79723d7271808a72d2
Aviral09 <aviralgoel2001@gmail.com>
2020-08-08 19:06:45 +0530

    Frontend templates for gate security

---
+++ swd/gate_security/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+from django.views.generic import TemplateView
+
+
+from . import views
+
+urlpatterns = [
+    
+url(r'^gate_security/', views.gate_security, name='gate_security'),
+url(r'^gate_student/', views.gate_student, name='gate_student')
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ swd/gate_security/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def gate_security(request):
+    
+    return render(request, "gate_security.html")
+
+def gate_student(request):
+    
+    return render(request, "gate_student.html")
