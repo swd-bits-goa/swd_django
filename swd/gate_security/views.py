d5f2bde318a1e4d824bb9d08f42b1849e3d3790a
raghav_dhir <raghav@musicperk.com>
2020-08-22 23:34:02 +0530

    added security views

--- swd/gate_security/views.py
+++ swd/gate_security/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render
+from .models import SecurityLeave
+from main.models import *
+
 
 # Create your views here.
 
 def gate_security(request):
-    
-    return render(request, "gate_security.html")
+    leaves = Leave.objects.filter(approved=True)
+    daypasss = DayPass.objects.filter(approved=True)
+    context = {}
+    found_leave = False
+    if request.POST:
+        username = request.POST.get('username')
+        for leave in leaves:
+            if username in leave.student.bitsId:
+                student = leave.student
+                found_leave = True
+                context = {
+                    'student': student,
+                    'leave': leave,
+                    'found_leave': found_leave
+                }
+        for daypass in daypasss:
+            if username in daypass.student.bitsId:
+                student = daypass.student
+                found_daypass = True
+                context = {
+                    'student': student,
+                    'daypass': daypass,
+                    'found_daypass': found_daypass,
+                    'found_leave': found_leave
+                }
 
+    return render(request, "gate_security.html", context)
