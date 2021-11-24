52af09ae3296e1e83d3a72798a9d93baee3c87f6
Khizir Siddiqui <khizirsiddiqui@gmail.com>
2021-11-24 19:12:28 +0530

    init commit

--- swd/gate_security/urls.py
+++ swd/gate_security/urls.py
@@ -8,13 +8,12 @@ from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    
-
-url(r'^gate_security/$', views.gate_security, name='gate_security'),
-url(r'^gate_security/in_out/$', views.in_out, name='in_out'),
-url(r'^gate_security/daypasses_security/$', views.dash_security_daypass, name='daypasses_security'),
-url(r'^gate_security/security_leaves/$', views.dash_security_leaves, name='dash_security_leaves'),
-url(r'^gate_security/daypass_out/$', views.daypass_out, name='daypass_out'),
-url(r'^gate_security/leave_out/$', views.leave_out, name='leave_out'),
+    url(r'^gate_security/$', views.gate_security, name='gate_security'),
+    url(r'^gate_security/in_out/$', views.in_out, name='in_out'),
+    url(r'^gate_security/daypasses_security/$', views.dash_security_daypass, name='daypasses_security'),
+    url(r'^gate_security/security_leaves/$', views.dash_security_leaves, name='dash_security_leaves'),
+    url(r'^gate_security/daypass_out/$', views.daypass_out, name='daypass_out'),
+    url(r'^gate_security/leave_out/$', views.leave_out, name='leave_out'),
+    url(r'^gate_security/weekend_security/$', views.dash_security_weekendpass, name='weekend_security'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- swd/gate_security/views.py
+++ swd/gate_security/views.py
@@ -1,19 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required, user_passes_test
-from .models import *
+from .models import InOut, WeekendPass
 from main.models import Leave, DayPass, Student
 from django.views.decorators.csrf import csrf_protect
 from django.conf import settings
+from django.db.models import Q
 from django.contrib.auth.models import User
 from main.templatetags.main_extras import is_hostelsuperintendent, is_warden, is_security, get_base_template
 import swd.config as config
 from datetime import date, datetime, timedelta, time
 
 
-# Create your views here.
-# @user_passes_test(lambda u: u.is_superuser)
+@user_passes_test(lambda u: u.is_superuser or is_security(u))
 def gate_security(request):
-
     context = {}
     errors = []
     if request.method == 'POST':
@@ -22,30 +21,45 @@ def gate_security(request):
             try:
                 student = Student.objects.get(bitsId=username)
                 errors = []
-            except:
+            except Student.DoesNotExist:
                 student = None
-                errors.append("Please enter correct BITSID")
+                errors.append("Please enter correct BITS ID")
 
             try:
                 inout = InOut.objects.get(student__bitsId=username)
-            except:
+            except InOut.DoesNotExist:
                 inout = None
 
             try:
-                t = time(0,0)
-                t1 = time(23,59)
-                d = date.today()
-                daypass = DayPass.objects.get(approved__exact=True, dateTime__date__exact=datetime.today().date(), student__bitsId = username, claimed = False)
-            except:
+                daypass = DayPass.objects.get(
+                        approved__exact=True,
+                        dateTime__date__exact=datetime.today().date(),
+                        student__bitsId=username,
+                        claimed=False
+                )
+            except DayPass.DoesNotExist:
                 daypass = None
 
             try:
                 t = time(0,0)
                 t1 = time(23,59)
                 d = date.today()
-                leave = Leave.objects.get(dateTimeStart__gte=datetime.combine(d,t), dateTimeStart__lte=datetime.combine(d,t1), student__bitsId = username, claimed = False)
-            except:
+                leave = Leave.objects.get(
+                        dateTimeStart__gte=datetime.combine(d,t),
+                        dateTimeStart__lte=datetime.combine(d,t1),
+                        student__bitsId=username,
+                        claimed=False
+                )
+            except Leave.DoesNotExist:
                 leave = None
+            
+            try:
+                weekendpass = WeekendPass.objects.get(
+                        student__bitsId=username,
+                        expiryDate__gte=datetime.today().date(),
+                        approved=True)
+            except WeekendPass.DoesNotExist:
+                weekendpass = None
 
             context = {
                 'student': student,
@@ -63,41 +77,55 @@ def gate_security(request):
             leave_check = request.POST.get('leave_check')
             daypass_check = request.POST.get('daypass_check')
             incampus_check = request.POST.get('incampus_check')
+            weekendpass_check = request.POST.get('weekendpass_check')
 
             try:
                 inout = InOut.objects.get(student__bitsId=username)
-            except:
+            except InOut.DoesNotExist:
                 inout = None
 
             t = time(0,0)
             t1 = time(23,59)
             d = date.today()
             try:
-                leave = Leave.objects.get(dateTimeStart__gte=datetime.combine(d,t), dateTimeStart__lte=datetime.combine(d,t1), student__bitsId = username, claimed = False)                  
-            except:
-                leave = None
-            try:
-                if(leave == None):
-                    leave = Leave.objects.get(student__bitsId = username, inprocess = True)
-            except:
+                leave = Leave.objects.get(
+                            (
+                                Q(dateTimeStart__gte=datetime.combine(d,t)) &
+                                Q(dateTimeStart__lte=datetime.combine(d,t1)) &
+                                Q(claimed=False)
+                            ) |
+                            Q(inprocess=True),
+                            student__bitsId=username
+                )
+            except Leave.DoesNotExist:
                 leave = None
 
-            
             try:
-                daypass = DayPass.objects.get(approved__exact=True, dateTime__date__exact=datetime.today().date(), student__bitsId = username, claimed = False)
-            except:
+                daypass = DayPass.objects.get(
+                        (
+                            Q(approved__exact=True) &
+                            Q(dateTime__date__exact=datetime.today().date()) &
+                            Q(claimed=False)
+                        ) |
+                        Q(inprocess=True),
+                        student__bitsId=username
+                )
+            except DayPass.DoesNotExist:
                 daypass = None
 
             try:
-                if(daypass == None):
-                    daypass = DayPass.objects.get(student__bitsId = username, inprocess = True)
-            except:
-                daypass = None
+                weekendpass = WeekendPass.objects.get(
+                        student__bitsId=username,
+                        expiryDate__gte=datetime.today().date(),
+                        approved=True)
+            except WeekendPass.DoesNotExist:
+                weekendpass = None
 
             if inout:
-                if inout.inCampus==True:
-                    inout.place=place
-                    inout.inCampus=False
+                if inout.inCampus == True:
+                    # Student is leaving campus
+                    inout.place = place
+                    inout.inCampus = False
                     inout.outDateTime = datetime.now()
                     inout.inDateTime = None
                     inout.save()
@@ -108,32 +136,40 @@ def gate_security(request):
                         leave.inprocess = True
                         leave.save()
 
-                    if daypass_check:
+                    elif daypass_check:
                         inout.onDaypass = True
                         inout.save()
                         daypass.inprocess = True
                         daypass.save()
 
+                    elif weekendpass_check:
+                        inout.onWeekendPass = True
+                        inout.save()
+
                 else:
-                    inout.place=place
-                    inout.inCampus=True
+                    inout.place = place
+                    inout.inCampus = True
                     inout.inDateTime = datetime.now()
                     inout.outDateTime = None
                     if inout.onLeave == True:
-                        inout.onLeave = False
                         leave.inprocess = False
                         leave.claimed = True
                         leave.save()
                     if inout.onDaypass == True:
-                        inout.onDaypass = False
                         daypass.inprocess = False
                         daypass.claimed = True
                         daypass.save()
                     inout.save()
             else:
-
-                inout = InOut(student=student, place=place, inDateTime=datetime.now(), outDateTime=datetime.now(), inCampus=False, onLeave=False, onDaypass=False)
-
+                inout = InOut(
+                            student=student,
+                            place=place,
+                            inDateTime=datetime.now(),
+                            outDateTime=datetime.now(),
+                            inCampus=False,
+                            onLeave=False,
+                            onDaypass=False
+                )
                 if not incampus_check:
                     inout.inCampus=False
                     inout.outDateTime = datetime.now()
@@ -168,19 +204,16 @@ def gate_security(request):
                         daypass.claimed = True
                         daypass.save()
                     inout.save()
-
             context = {
                 'student': student,
                 'inout': inout,
                 'success': True,
             }
-
             return render(request, "gate_security.html", context)
-
     return render(request, "gate_security.html", context)
 
 
-@user_passes_test(is_security)
+@user_passes_test(lambda u: u.is_superuser or is_security(u))
 def dash_security_leaves(request):
     t = time(0,0)
     t1 = time(23,59)
@@ -190,7 +223,7 @@ def dash_security_leaves(request):
     return render(request, "dash_security.html", context)
 
 
-@user_passes_test(is_security)
+@user_passes_test(lambda u: u.is_superuser or is_security(u))
 def dash_security_daypass(request):
     t = time(0,0)
     t1 = time(23,59)
@@ -199,27 +232,35 @@ def dash_security_daypass(request):
     context = {'daypasses' : approved_daypass}
     return render(request, "daypasses_security.html", context)
 
-# @user_passes_test(lambda u: u.is_superuser)
+@user_passes_test(lambda u: u.is_superuser or is_security(u))
 def in_out(request):
     inout = InOut.objects.filter(inCampus = False, onLeave = False, onDaypass = False).order_by('-outDateTime')
     context = {
         'inout': inout,
     }
-
     return render(request, "all_in_out.html",context)
 
-@user_passes_test(is_security)
+@user_passes_test(lambda u: u.is_superuser or is_security(u))
 def leave_out(request):
     inout = InOut.objects.filter(inCampus = False, onLeave = True, onDaypass = False).order_by('-outDateTime')
     context = {
         'inout': inout,
     }
-    return render(request, "leave_out.html",context)
+    return render(request, "leave_out.html", context)
 
-@user_passes_test(is_security)
+@user_passes_test(lambda u: u.is_superuser or is_security(u))
 def daypass_out(request):
     inout = InOut.objects.filter(inCampus = False, onLeave = False, onDaypass = True).order_by('-outDateTime')
     context = {
         'inout': inout,
     }
-    return render(request, "daypass_out.html",context)
+    return render(request, "daypass_out.html", context)
+
+@user_passes_test(lambda u: u.is_superuser or is_security(u))
+def dash_security_weekendpass(request):
+    approved_weekend = WeekendPass.objects.filter(
+            approved=True,
+            expiryDate__gte=datetime.today().date()
+    )
+    context = {'weekend_pass' : approved_weekend}
+    return render(request, "weekend_security.html", context)
