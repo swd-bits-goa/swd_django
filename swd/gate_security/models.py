fe8a9c798fa8f86423b85c3350ddfba3e42f92ff
Aviral Kumar Goel <59691764+Aviral09@users.noreply.github.com>
2021-11-25 15:45:13 +0530

    add weekend pass
    * init commit
    
    * Add weekend pass
    
     - Does not include Weekend Pass Student Application
    Co-authored-by: Khizir Siddiqui <khizirsiddiqui@gmail.com>

--- swd/gate_security/models.py
+++ swd/gate_security/models.py
@@ -3,8 +3,6 @@ from django.utils import timezone
 from datetime import datetime
 from datetime import date
 
-# Create your models here.
-
 class InOut(models.Model):
     student = models.ForeignKey('main.Student', on_delete = models.CASCADE)
     place = models.CharField(max_length=20, null=True, blank=True)
@@ -13,6 +11,25 @@ class InOut(models.Model):
     inCampus = models.BooleanField(null=False, default=True)
     onLeave = models.BooleanField(null=False, default=False)
     onDaypass = models.BooleanField(null=False, default=False)
+    onWeekendPass = models.BooleanField(null=False, default=False)
 
     def __str__(self):
         return self.student.bitsId + ' (' + self.student.name + ')'
+
+
+class WeekendPass(models.Model):
+    """
+    Stores Approved Weekend Passes issued by SWD Office
+    """
+    student = models.ForeignKey('main.Student', on_delete=models.CASCADE)
+    expiryDate = models.DateField(null=False)
+    approved = models.BooleanField(default=False)
+    place = models.CharField(max_length=20, blank=False)
+
+    class Meta: 
+        verbose_name = "Weekend Pass"
+        verbose_name_plural = "Weekend Passes"
+
+    def __str__(self):
+        return self.student.bitsId + ': ' + str(self.expiryDate)
+
