55acefd17323f6f94d7d96d2aed2dbe3a1152741
Khizir Siddiqui <khizirsiddiqui@gmail.com>
2021-12-07 12:41:16 +0530

    Remove ignored files

--- swd/swd/config.py
+++
@@ -1,5 +0,0 @@
-PRODUCTION = True
-DB_NAME = 'swd' if PRODUCTION == True else ''
-DB_USER = 'swduser' if PRODUCTION == True else ''
-DB_PASSWORD = 'swduser' if PRODUCTION == True else ''
-EMAIL_PROD = True
