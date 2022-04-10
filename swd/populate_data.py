0856431d4f4657c1b7a5da916d666d1cb90fa47c
robo-mop <realaryanpingle@gmail.com>
2022-04-10 14:54:27 +0530

    Added support for dual degree students (both with and without the B.E. degree)

--- swd/populate_data.py
+++ swd/populate_data.py
@@ -15,9 +15,27 @@ import random
 import datetime
 import argparse
 
+"""
+Running this script will create a dummy population. For convenience, here's the command line statement to create a small dataset:
+$ python populate_data.py --dataset_size small
+
+Here are some dummy credentials that will be created, all with the password 'password':
+admin
+    The superuser
+warden_<hostel name in lowercase>
+    The warden of a hostel
+superintendent_<hostel_choices[i]>_<hostel_choices[i+1]>
+    A superintendent in charge of hostels `hostel_choices[i+1]` and `hostel_choices[i+1]`. Note that the superintendent for the last hostel (DH6 as of writing this) may be in charge of only the last one in case the number of hostels is odd. All other superintendents are given by two consecutive hostels in `hostel_choices`
+    eg: superintendent_AH1_AH2, superintendent_CH2_CH3 etc.
+security0
+    The main gate security
+"""
+
 boolen_choices = [True, False]
 gender_choices = ['M', 'F']
 branch_choices = ['A1', 'A3', 'A4', 'A7', 'A8', 'B1', 'B2', 'B3', 'B4', 'B5']
+single_degree_choices = ['A1', 'A3', 'A4', 'A7', 'A8']
+dual_degree_choices = ['B1', 'B2', 'B3', 'B4', 'B5']
 higher_degree_branch_choices = ["H101", "H103", "H112", "H123", "H129", "H140", "H141", "H106", "H151", "H152"]
 hostel_choices = ['AH1', 'AH2', 'AH3', 'AH4', 'AH5', 'AH6', 'AH7', 'AH8', 'AH9',
                   'CH1', 'CH2', 'CH3', 'CH4', 'CH5', 'CH6', 'CH7',
@@ -73,19 +91,30 @@ def fake_status():
 
 # username for the below functions will be of the format `[pf]\d{8}`
 
-def fake_bitsID(username):
+def fake_singlite(username):
     year = username[1:5]
     uid = username[5:]
-    branch = str(random.choice(branch_choices))
+    branch = str(random.choice(single_degree_choices))
     return f"{year}{branch}PS{uid}G"
 
+def fake_dulla_firstie(username):
+    year = username[1:5]
+    uid = username[5:]
+    branch = str(random.choice(dual_degree_choices))
+    return f"{year}{branch}PS{uid}G"
+
+def fake_dulla_secondie(username):
+    year = username[1:5]
+    uid = username[5:]
+    branch1 = str(random.choice(dual_degree_choices))
+    branch2 = str(random.choice(single_degree_choices))
+    return f"{year}{branch1}{branch2}{uid}G"
 
 def fake_phd(username):
     year = username[1:5]
     uid = username[5:]
     branch = 'PHX' + str(random.choice(['P', 'F']))
-    return str(year + branch + uid + 'G')
-
+    return f"{year}{branch}{uid}G"
 
 def fake_higher_degree(username):
     year = username[1:5]
@@ -104,36 +133,55 @@ def create_super_user():
     except:
         print("Error occurred while creating superuser.")
 
+@transaction.atomic
+def create_generic_users(users:list, password:str="password"):
+    """
+    Creates and saves a list of users as given in the params.
+    This function uses transaction.atomic, so all database operations in this function will be completed before returning
+
+    :param users: A list of dicts with three keys - 'username', 'first-name', 'email'
+    :param password: The password given to each user of the list (default = 'password')
+    """
+    user_list = [None]*(len(users))
+    counter = 0
+    for user in users:
+        user = User(
+            username = user["username"],
+            first_name = user["first-name"],
+            email = user["email"]
+        )
+        user_list[counter] = user
+        user.set_password(password)
+        counter += 1
+    # Bulk create for performance
+    User.objects.bulk_create(user_list)
+
+    # Return the newly created users
+    user_objects = User.objects.all()
+    return user_objects[len(user_objects) - len(users):]
+
 def create_students_list(PER_BATCH_SIZE, YEAR_START, YEAR_END):
     """Creates a list of Users, then maps a Student to each User"""
 
     # First, generate the list of student users
-    mUser_list = [None]*(PER_BATCH_SIZE * (YEAR_END - YEAR_START + 1))
+    # mUser_list = [None]*(PER_BATCH_SIZE * (YEAR_END - YEAR_START + 1))
+    N = PER_BATCH_SIZE * (YEAR_END - YEAR_START + 1)
     function_prefix_list = []
     stime = time.time()
     print("Creating Student Users...", end=" ")
-    counter = 0
+    user_data = []
     for year in range(YEAR_START, YEAR_END+1):
         for id in range(PER_BATCH_SIZE):
             i = year*10000 + id*10000//PER_BATCH_SIZE
-            [fake_id_function, prefix] = [[fake_phd, 'p'], [fake_bitsID, 'f'], [fake_higher_degree, 'h']][random.randint(0, 2)]
+            [fake_id_function, prefix] = random.choice([[fake_phd, 'p'], [fake_singlite, 'f'], [fake_dulla_firstie, 'f'], [fake_dulla_secondie, 'f'], [fake_higher_degree, 'h']])
             function_prefix_list.append([fake_id_function, prefix])
-            mUser = User(
-                username = f"{prefix}{i}",
-                first_name = f"Student{i}",
-                email = f"{prefix}{i}@goa.bits-pilani.ac.in"
-            )
-            mUser.set_password('password')
-            mUser_list[counter] = mUser
-
-            counter += 1
-        
-    # Save the list of users
-    with transaction.atomic():
-        User.objects.bulk_create(mUser_list)
-    print(f"{len(mUser_list)} created in {time.time() - stime:.3f}s")
-
-    mUser_list = User.objects.all()
+            user_data.append({
+                "username": f"{prefix}{i}",
+                "first-name": f"Student{i}",
+                "email": f"{prefix}{i}@goa.bits-pilani.ac.in"
+            })
+    user_list = create_generic_users(user_data)        
+    print(f"{len(user_list)} created in {time.time() - stime:.3f}s")
 
     # Next, generate a list of students
     students_list = []
@@ -143,7 +191,7 @@ def create_students_list(PER_BATCH_SIZE, YEAR_START, YEAR_END):
     for year in range(YEAR_START, YEAR_END+1):
         for id in range(PER_BATCH_SIZE):
             i = year*10000 + id*10000//PER_BATCH_SIZE
-            mUser = mUser_list[counter]
+            mUser = user_list[counter]
             [fake_id_function, prefix] = function_prefix_list[counter]
             mStudent = Student(
                 user=mUser,
@@ -176,22 +224,16 @@ def create_wardens():
     user_list = []
     stime = time.time()
     print("Creating Warden Users...", end=" ")
-    for i in range(len(hostel_choices)):
-        user = User(
-            username='warden' + str(i),
-            first_name='Warden' + str(i),
-            email=f"warden{i}@goa.bits-pilani.ac.in"
-        )
-        user.set_password('password')
-        user_list.append(user)
-
-    # Save the list of users
-    with transaction.atomic():
-        User.objects.bulk_create(user_list)
+    user_data = [
+        {
+            "username": f'warden_{hostel_choices[i]}',
+            "first-name": f'Warden_{hostel_choices[i]}',
+            "email": f"warden_{hostel_choices[i]}@goa.bits-pilani.ac.in"
+        }
+        for i in range(len(hostel_choices))
+    ]
+    user_list = create_generic_users(user_data)
     print(f"{len(user_list)} created in {time.time() - stime:.3f}s")
-
-    user_list = User.objects.all()
-    user_list = user_list[len(user_list) - len(hostel_choices):]
 
     # Next, generate a list of wardens
     warden_list = []
@@ -307,11 +349,11 @@ def create_leaves(student_list):
                 student=student,
                 dateTimeStart=timezone.now() + datetime.timedelta(days=1),
                 dateTimeEnd=timezone.now() + datetime.timedelta(days=5),
-                reason='SomeRandomEventDescription',
+                reason='Random Event Description For Leave Application',
                 consent='Email',
                 corrAddress='Earth',
                 corrPhone=fake_number_generator(),
-                approvedBy=warden,
+                approvedBy=status and warden,
                 approved=status,
                 disapproved=not status,
                 inprocess=not status,
@@ -349,25 +391,18 @@ def create_securities(number=10):
     """Creates a list of Users, then maps a Security to each User"""
 
     # First, generate the list of security users
-    user_list = []
     stime = time.time()
     print("Creating Security Users...", end=" ")
-    for i in range(number):
-        user = User(
-            username=f'security{i}',
-            first_name=f'security{i}',
-            email=f"security{i}@goa.bits-pilani.ac.in"
-        )
-        user.set_password('password')
-        user_list.append(user)
-        
-    # Save the list of users
-    with transaction.atomic():
-        User.objects.bulk_create(user_list)
+    user_data = [
+        {
+            "username": f'security{i}',
+            "first-name": f'security{i}',
+            "email": f"security{i}@goa.bits-pilani.ac.in"
+        }
+        for i in range(number)
+    ]
+    user_list = create_generic_users(user_data)
     print(f"{len(user_list)} created in {time.time() - stime:.3f}s")
-
-    user_list = User.objects.all()
-    user_list = user_list[len(user_list) - number:]
 
     # Next, generate a list of securities
     security_list = []
@@ -389,29 +424,22 @@ def create_hostelsuperintendents():
 
     # One Superintendent can be in charge of multiple hostels, so we need a nice way to generate some permutations
     # But I'm not smart enough for that right now, so I'll just make one superintendent in charge of two consecutive hostels
-    combinations = [hostel_choices[i:i+2] for i in range(len(hostel_choices)-1)]
+    combinations = [hostel_choices[i:i+2] for i in range(0, len(hostel_choices), 2)]
     superintendent_jurisdictions = [", ".join(combination) for combination in combinations]
 
     # First, generate the list of superintendent users
-    user_list = []
     stime = time.time()
     print("Creating Superintendent Users...", end=" ")
-    for i in range(len(superintendent_jurisdictions)):
-        user = User(
-            username='superintendent' + str(i),
-            first_name='superintendent' + str(i),
-            email='superintendent' + str(i) +"@goa.bits-pilani.ac.in"
-        )
-        user.set_password('password')
-        user_list.append(user)
-        
-    # Save the list of users
-    with transaction.atomic():
-        User.objects.bulk_create(user_list)
+    user_data = [
+        {
+            "username": f'superintendent_{"_".join(combinations[i])}',
+            "first-name": f'superintendent_{"_".join(combinations[i])}',
+            "email": f'superintendent_{"_".join(combinations[i])}@goa.bits-pilani.ac.in'
+        }
+        for i in range(len(superintendent_jurisdictions))
+    ]
+    user_list = create_generic_users(user_data)
     print(f"{len(user_list)} created in {time.time() - stime:.3f}s")
-
-    user_list = User.objects.all()
-    user_list = user_list[len(user_list) - len(superintendent_jurisdictions):]
 
     # Next, generate a list of superintendents
     superintendent_list = []
@@ -499,7 +527,7 @@ if __name__ == '__main__':
     create_bonafides(students_list)
     create_leaves(students_list)
     create_csas(students_list)
-    create_securities(number=10)
+    create_securities(number=1) # Only one gate security - security0
     create_hostelsuperintendents()
     create_notices(number=15)
 
