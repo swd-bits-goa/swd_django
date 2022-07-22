b67dbf69cd396081130744fc59b6eee3045aad8b
robo-mop <realaryanpingle@gmail.com>
2022-07-22 18:47:43 +0530

    Added optional 'as on date' for bonafides

--- swd/main/models.py
+++ swd/main/models.py
@@ -295,12 +295,14 @@ class Bonafide(models.Model):
         </p>
         '''.strip()
 
+        reason_is_passport = reason.lower() == "passport"
+
         if(res.status == "Student"):
             return f'''
             <p>
-                This is to certify that <i>{honorific} {self.student.name.upper()}</i>, ID No. <i>{self.student.bitsId}</i> is a bonafide student of {yearName} year class. {pronoun} was admitted to the Institute on {str(date_admit)}, for pursuing the <i>{branch}</i> {"under dual degree " if dual_degree_student else ""}programme of studies. {pronoun} is residing in the Hostel <i>{res.hostel}-{res.room}</i> of this Institute as on date.
+                This is to certify that <i>{honorific} {self.student.name.upper()}</i>, ID No. <i>{self.student.bitsId}</i> is a bonafide student of {yearName} year class. {pronoun} was admitted to the Institute on {str(date_admit)}, for pursuing the <i>{branch}</i> {"under dual degree " if dual_degree_student else ""}programme of studies. {pronoun} is residing in the Hostel <i>{res.hostel}-{res.room}</i> of this Institute{" as on date" if reason_is_passport else ""}.
             </p>
-            { address_html_for_passport if reason == "Passport" else ""}
+            {address_html_for_passport if reason_is_passport else ""}
             <p>
                 This certificate is issued for the purpose of applying for {reason}.
             </p>
@@ -311,7 +313,7 @@ class Bonafide(models.Model):
             <p>
                 This is to certify that <i>{honorific} {self.student.name.upper()}</i>, ID No. <i>{self.student.bitsId}</i> is a bonafide student of {yearName} year class. {pronoun} was admitted to the Institute on {str(date_admit)}, for pursuing the <i>{branch}</i> {"under dual degree " if dual_degree_student else ""}programme of studies. {pronoun} is pursuing <i>{res.status}</i> at <i>{res.psStation}</i>.
             </p>
-            { address_html_for_passport if reason == "Passport" else ""}
+            {address_html_for_passport if reason_is_passport else ""}
             <p>
                 This certificate is issued for the purpose of applying for {reason}.
             </p>
