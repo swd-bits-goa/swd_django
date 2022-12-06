0dbd142595fe0bec2be4854f4854e39d3e7c8cf8
robo-mop <realaryanpingle@gmail.com>
2022-12-06 11:22:35 +0530

    Add InOut Export

--- swd/gate_security/admin.py
+++ swd/gate_security/admin.py
@@ -1,10 +1,18 @@
 # Register your models here.
 from django.contrib import admin
+from import_export.formats import base_formats
+from import_export.admin import ExportMixin
 from gate_security.models import InOut, WeekendPass
 
 
-class InOutAdmin(admin.ModelAdmin):
+class InOutAdmin(ExportMixin, admin.ModelAdmin):
     search_fields = ['student__name','student__bitsId', 'inDateTime', 'onLeave', 'onDaypass']
+
+    def get_export_formats(self):
+        formats = (
+            base_formats.XLS,
+        )
+        return [f for f in formats if f().can_export()]
 
 class WeekendPassAdmin(admin.ModelAdmin):
     search_fields = ['student__name', 'student__bitsId', 'expiryDate']
