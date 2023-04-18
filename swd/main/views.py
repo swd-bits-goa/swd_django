9ec58ee653439f3e69147a8a63010cf0748bd130
robo-mop <realaryanpingle@gmail.com>
2023-04-18 11:21:19 +0530

    Add update_names portal

--- swd/main/views.py
+++ swd/main/views.py
@@ -3186,6 +3186,57 @@ def update_ids(request):
     return render(request, "add_students.html", {'header': "Update IDs"})
 
 @user_passes_test(lambda u: u.is_superuser)
+def update_names(request):
+    message_str = ''
+    message_tag = messages.INFO
+    if request.POST:
+        if request.FILES:
+            # Read Excel File into a temp file
+            xl_file = request.FILES['xl_file']
+            extension = xl_file.name.rsplit('.', 1)[1]
+            if ('xls' != extension):
+                if ('xlsx' != extension):
+                    messages.error(request, "Please upload .xls or .xlsx file only")
+                    messages.add_message(request,
+                                        message_tag, 
+                                        message_str)
+                    return render(request, "add_students.html", {'header': "Update Names"})
+
+            fd, tmp = tempfile.mkstemp()
+            with os.fdopen(fd, 'wb') as out:
+                out.write(xl_file.read())
+            workbook = xlrd.open_workbook(tmp)
+
+            count = 0
+            idx = 1
+            header = {}
+            for sheet in workbook.sheets():
+                for row in sheet.get_rows():
+                    if idx == 1:
+                        col_no = 0
+                        for cell in row:
+                            # Store the column names in dictionary
+                            header[str(cell.value)] = col_no
+                            col_no = col_no + 1
+                        idx = 0
+                        continue
+                    
+                    Student.objects.filter(
+                        bitsId=row[header['studentID']].value
+                        ).update(name=str(row[header['Name']].value))
+                    
+                    count = count + 1
+            message_str = str(count) + " Updated students' names"
+        else:
+            message_str = "No File Uploaded."
+
+    if message_str is not '':
+        messages.add_message(request,
+                            message_tag, 
+                            message_str)
+    return render(request, "add_students.html", {'header': "Update Names"})
+
+@user_passes_test(lambda u: u.is_superuser)
 def update_ps(request):
     message_str = ''
     message_tag = messages.INFO
